439aa9aba0f8aabf03920f9e6392fee4e28e7633
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-03-08 15:19:23 +0100

    feat: Show recipes from the database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Recipe;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    $recipes = Recipe::paginate(5);
+
+    return view('index', [
+        'recipes' => $recipes
+    ]);
 });
