cad889a4ce345d6f3c541a75e6bed7384c02f64d
Aleksandar Vranesevic <bg.aleksandar@gmail.com>
2023-05-30 20:27:09 +0200

    style: Add registration link to the navigation

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -26,6 +26,9 @@
                     <li class="nav-item">
                         <a class="nav-link text-light" href="/login">Login</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light" href="/register">Register</a>
+                    </li>
                 @endauth
             </ul>
             <form class="d-flex mb-0" role="search">
